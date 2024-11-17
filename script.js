a9fe3a0fab5759ea03dd1faa31e3acc1b965285b
Alexandre Koch <85593242+xandekoch@users.noreply.github.com>
2024-11-17 18:14:33 -0300

    feat: Added animation on scroll.

---
+++ script.js
@@ -0,0 +1,12 @@
+const elements = document.querySelectorAll('h1, h2, button');
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+            observer.unobserve(entry.target);
+        }
+    });
+}, { threshold: 0.5 });
+
+elements.forEach(element => observer.observe(element));
